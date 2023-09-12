acac55a9cdb148da2daeedd40d169db16fd29d68
Jeremy Sobczak <code@jerspace.com>
2023-09-12 14:29:18 -0400

    Fixed error handling problem in bgg-api. Added redis as new dependency.

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(function (req, res, next) {
 
 /** Generic error handler; anything unhandled goes here. */
 app.use(function (err, req, res, next) {
-  if (process.env.NODE_ENV !== "test") console.error(err.stack);
+  if (process.env.NODE_ENV !== "test") console.error(err);
   const status = err.status || 500;
   const message = err.message;
 
